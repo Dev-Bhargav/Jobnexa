97864c0d4ad040f02e4c0c1d922767fff701ef60
Bhargav <bhargavdinesh5406@gmail.com>
2023-12-13 07:35:41 +0530

    backup: before mail chimp integration

--- app/ui/Detail.tsx
+++ app/ui/Detail.tsx
@@ -17,29 +17,6 @@ export default async function Detail(props: { id: string }) {
           dangerouslySetInnerHTML={{ __html: html }}
         ></div>
       ))}
-      <table className="[&_td]:w-96 [&_td]:leading-5 [&_td]:px-4 [&_td]:py-2 max-w-full">
-        <thead>
-          <tr>
-            <th colSpan={2} className="py-2 px-4 border text-start text-xl">
-              Details
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="border-x">
-            <td className="font-medium">Post</td>
-            <td>SBI Clerk</td>
-          </tr>
-          <tr className="border-x">
-            <td className="font-medium">Date</td>
-            <td>17-11-2023</td>
-          </tr>
-          <tr className="border-x border-b">
-            <td className="font-medium">Total Vacancy </td>
-            <td>100</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
