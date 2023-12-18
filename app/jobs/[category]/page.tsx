08825e6f3c1c3c06e8eac9c905584a7734c060a7
Bhargav <bhargavdinesh5406@gmail.com>
2023-12-18 10:06:05 +0530

    feat: SSG

--- app/jobs/[category]/page.tsx
+++ app/jobs/[category]/page.tsx
@@ -1,22 +1,32 @@
 import Quicknav from "@/app/ui/Quicknav";
 import TypeBlog from "@/app/ui/TypeBlog";
 
+export const revalidate = 15
+
 export default function Home({
-    params,
-  }: {
-    params: {
-      category: string;
-    };
-  }) {
+  params,
+}: {
+  params: {
+    category: string;
+  };
+}) {
   return (
     <>
       <div className="w-4/5 max-w-[920px] min-w-[330px] xl:w-3/4 xl:px-10 flex gap-3 flex-col">
         <h1 className="text-4xl  text-[#2D2B2B] font-black my-4">
-         {params.category.charAt(0).toUpperCase() + params.category.slice(1)} Jobs
+          {params.category.charAt(0).toUpperCase() + params.category.slice(1)}{" "}
+          Jobs
         </h1>
-        <TypeBlog category={params.category}/>
+        <TypeBlog category={params.category} />
       </div>
       <Quicknav />
     </>
   );
+}
+
+export async function generateStaticParams() {
+  const jobsCat = ["govt", "railway", "defence", "bank"];
+  return jobsCat.map((category) => ({
+    category,
+  }));
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+
 export async function fetchJobs() {
   try {
     const jobs = await prisma.jobs.findMany({
