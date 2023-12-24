84350e82f1550c98039c6a588e411db0d878b472
Bhargav <bhargavdinesh5406@gmail.com>
2023-12-25 05:09:56 +0530

    fix: table with heading

--- lib/utils.ts
+++ lib/utils.ts
@@ -33,38 +33,73 @@ export function convertHtml(htmlarray: Array<htmlObjectType>) {
         return `<p>${html.data.text}</p>`;
       case "table":
         if (html.data.withHeadings) {
+          let headers = html.data.content[0];
+          headers = headers.filter((value: string) => value !== "");
+
+          const content = html.data.content;
+          content.shift();
+
+          const table = `<table class="border-collapse border border-slate-400 bg-white text-md shadow-sm">
+          <thead>
+            <tr>
+              ${headers
+                .map(
+                  (head: string) =>
+                    `<td class="border py-3 px-3 font-medium text-lg border-slate-300">${head}</td>`
+                )
+                .join("")}
+            </tr>
+          </thead>
+          <tbody >
+          ${content
+            .map((row: Array<string>, rowIdx: Number) => {
+              const rows = `<tr>
+             ${row
+               .map(
+                 (headers) =>
+                   `<td key=${rowIdx} class="border py-2 px-3  text-[#262626] border-slate-300" >${headers}</td>`
+               )
+               .join(" ")}
+             </tr>
+            `;
+              return rows;
+            })
+            .join(" ")}
+          </tbody>
+        </table>`;
+          return table;
+        } else {
+          const content = html.data.content;
+          const table = `<table class="w-full border-collapse border border-slate-400 bg-white text-md shadow-sm">
+              <tbody >
+              ${content
+                .map((row: Array<string>, rowIdx: Number) => {
+                  const rows = `<tr>
+                 ${row
+                   .map(
+                     (values) =>
+                       `<td key=${rowIdx} class="border py-2 px-3 text-[#262626] border-slate-300" >${values}</td>`
+                   )
+                   .join(" ")}
+                 </tr>
+                `;
+                  return rows;
+                })
+                .join(" ")}
+              </tbody>
+            </table>`;
+          return table;
         }
-        //! Make Proper Error
-        const content = html.data.content;
-        const table = `<table class="w-full border-collapse border border-slate-400 bg-white text-md shadow-sm">
-            <tbody >
-            ${content
-              .map((row: Array<string>, rowIdx: Number) => {
-                const rows = `<tr>
-               ${row
-                 .map(
-                   (values) =>
-                     `<td key=${rowIdx} class="border py-2 px-3 text-[#262626] border-slate-300" >${values}</td>`
-                 )
-                 .join(" ")}
-               </tr>
-              `;
-                return rows;
-              })
-              .join(" ")}
-            </tbody>
-          </table>`;
-        return table;
 
       case "image":
         return `<img src="${html.data.file.url}"  />`;
       case "list":
-        const list = html.data.items.map((listitem:string) =>{
+        const list = html.data.items.map((listitem: string) => {
           return `<ul class="ml-3">
             <li class="mx-4" style="list-style-type: disc">${listitem}</li>
-          </ul>`
-        })
-        return list
+          </ul>`;
+        });
+        return list;
       default:
         return "work";
     }
