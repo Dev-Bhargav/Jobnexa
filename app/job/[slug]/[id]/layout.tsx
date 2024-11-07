4d98630fce05be1a735f37d573c20449ee7b7398
SPIDY <bhargavdinesh5406@gmail.com>
2024-11-07 22:01:20 +0530

    fix: layout shifting

--- app/job/[slug]/[id]/layout.tsx
+++ app/job/[slug]/[id]/layout.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function ContentLayout({children}:{
-    children: React.ReactNode
+export default function ContentLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
-    <div className="w-4/5 max-w-[920px] min-w-[330px] xl:w-3/4 xl:px-10 flex gap-3 flex-col">
+    <div className="w-4/5 max-w-[920px] min-w-[330px] xl:w-3/4 xl:px-5 flex gap-3 flex-col">
       {children}
     </div>
-  )
+  );
 }
--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -4,7 +4,7 @@ export default function ContentLayout({children}:{
     children: React.ReactNode
 }) {
   return (
-    <div className="w-4/5 min-w-[920px] xl:w-3/4 xl:px-5 flex gap-3 flex-col">
+    <div className="w-4/5 max-w-[920px] min-w-[330px] xl:w-3/4 xl:px-5 flex gap-3 flex-col">
       {children}
     </div>
   )
